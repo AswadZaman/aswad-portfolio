5d5f22be3fd5ccc194e0af407744a9f152988a89
AswadZaman <aswadkaleem506@gmail.com>
2023-05-17 15:39:42 +0500

    Add boilerplate

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { ToastContainer, toast } from "react-toastify";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <ToastContainer />
+    </>
+  );
+}
+
+export default App;
